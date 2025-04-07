6eb48aacdc4216b84ab937c8041c4c4cc8445b71
Yousef tamer <youseftamereg@gmail.com>
2025-04-08 00:27:24 +0200

    the full website

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "team-shoky-helper-v1";
+const urlsToCache = [
+  "./index.html",
+  "./icon-192x192.png",
+  "./icon-512x512.png",
+  "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css",
+  "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.0.0/css/all.min.css",
+  "https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.min.css",
+  "https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.min.js",
+  "./youseftamer_profile.jpg",
+  "./ahmed_adel.jpeg",
+  "./ammar roshdy.jpg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache).catch((error) => {
+        console.error("Failed to cache resources:", error);
+      });
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
