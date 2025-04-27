6197cfad8af11f824e9a123b037b0de4ecd7443f
Yousef tamer <youseftamereg@gmail.com>
2025-04-27 15:57:02 +0300

    edit service

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
 */
 
 
-const CACHE_NAME = "team-shoky-helper-v5"; // <-- INCREMENTED VERSION!
+const CACHE_NAME = "team-shoky-helper-v6"; // <-- INCREMENTED VERSION!
 
 // Essential files for the App Shell to load offline
 const APP_SHELL_URLS = [
