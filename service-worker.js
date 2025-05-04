d5d4f221733928a34b54066abf2252bf0db1c001
Yousef tamer <youseftamereg@gmail.com>
2025-05-04 16:46:17 +0300

    update in mobile view

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
  Version: v9
 */
 
-const CACHE_NAME = "team-shoky-helper-v13"; // <-- INCREMENTED VERSION!
+const CACHE_NAME = "team-shoky-helper-v14"; // <-- INCREMENTED VERSION!
 
 // Essential files for the App Shell to load offline
 const APP_SHELL_URLS = [
