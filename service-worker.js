a089fa8b78e7e1653289d0f0403afe28508e0559
Yousef tamer <youseftamereg@gmail.com>
2025-05-04 17:31:07 +0300

    update vodafone

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
  Version: v9
 */
 
-const CACHE_NAME = "team-shoky-helper-v15"; // <-- INCREMENTED VERSION!
+const CACHE_NAME = "team-shoky-helper-v16"; // <-- INCREMENTED VERSION!
 
 // Essential files for the App Shell to load offline
 const APP_SHELL_URLS = [
